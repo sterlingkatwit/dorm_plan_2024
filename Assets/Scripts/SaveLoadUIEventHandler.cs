d5cf80feddae132786f8038e8e0903b9c1e446dd
unknown <ldesousa@wit.edu>
2024-07-01 14:07:43 -0400

    Added file sidebar

--- Assets/Scripts/SaveLoadUIEventHandler.cs
+++ Assets/Scripts/SaveLoadUIEventHandler.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
 
 public class SaveLoadUI : MonoBehaviour
 {
+    public Button save, load, saveas;
+    public bool isPressed = false;
+
     public void OnButtonPress()
     {
         string buttonName = EventSystem.current.currentSelectedGameObject.name;
-        if (buttonName.Equals("SaveLoadBtn"))
+        if (buttonName.Equals("SaveLoadBtn") && !isPressed)
         {
-
+            save.gameObject.SetActive(true);
+            saveas.gameObject.SetActive(true);
+            load.gameObject.SetActive(true);
+            isPressed = true;
+        }
+        else if (buttonName.Equals("SaveLoadBtn") && isPressed)
+        {
+            save.gameObject.SetActive(false);
+            saveas.gameObject.SetActive(false);
+            load.gameObject.SetActive(false);
+            isPressed = false;
         }
     }
 }
